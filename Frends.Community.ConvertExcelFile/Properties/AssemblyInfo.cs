9b74c5c3ab272cb78ab34fc65251fcd4dbc316ef
Jura Laakkonen <Jura.Laakkonen@hiq.fi>
2017-11-16 13:48:58 +0200

    FCOM-129: fixed assembly name (FRENDS -> Frends)

--- Frends.Community.ConvertExcelFile/Properties/AssemblyInfo.cs
+++ Frends.Community.ConvertExcelFile/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FRENDS.Community.ConvertExcelFile")]
+[assembly: AssemblyTitle("Frends.Community.ConvertExcelFile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FRENDS.Community.ConvertExcelFile")]
+[assembly: AssemblyProduct("Frends.Community.ConvertExcelFile")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Frends.Community.ConvertExcelFileTests/Properties/AssemblyInfo.cs
+++ Frends.Community.ConvertExcelFileTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FRENDS.Tests")]
+[assembly: AssemblyTitle("Frends.Community.ConvertExcelFileTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FRENDS.Tests")]
+[assembly: AssemblyProduct("Frends.Community.ConvertExcelFileTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
